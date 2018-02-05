0652b10a46b87ed2a4091d5ffc3000a8118b0fed
PyjterPL <piotr_zuber@interia.pl>
2018-02-05 15:08:31 +0100

    Dodane miasta

---
+++ Przychodnia/Obiekty Bazy/Miasto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Przychodnia.Obiekty_Bazy
+{
+    class Miasto
+    {
+        public int ID { get; private set; }
+        public string Nazwa { get; private set; }
+
+        public Miasto(int id, string nazwa)
+        {
+            this.ID = id;
+            this.Nazwa = nazwa;
+        }
+        public static List<Miasto> PobierzWszystkieMiasta()
+        {
+            int id;
+            string nazwa;          
+            var miasta = new List<Miasto>();
+
+            var zapytanie = "SELECT * FROM miasta";
+            var komenda = new MySqlCommand(zapytanie, DbHelper.Polaczenie);
+
+            DbHelper.Polaczenie.Open();
+
+            var reader = komenda.ExecuteReader();
+
+            while (reader.Read())
+            {
+                id = (int)reader["Id_miasta"];
+                nazwa = reader["Nazwa"].ToString();
+
+                var miasto = new Miasto(id, nazwa);
+
+                miasta.Add(miasto);
+            }
+            DbHelper.Polaczenie.Close();
+            return miasta;
+
+        }
+    }
+}
